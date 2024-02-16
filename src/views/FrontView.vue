884497087622f23097c57153742971ff6ec0828b
xuannn0915 <xuannn0915@gmail.com>
2024-02-16 18:09:35 +0800

    0216

--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-2">
                 <nav class="nav flex-column bg-body-tertiary">
-                    <RouterLink class="nav-item px-4 py-2" to="/home" >Home</RouterLink>
+                    <RouterLink class="nav-item px-4 py-2" to="/home">Home</RouterLink>
                     <RouterLink class="nav-item px-4 py-2" to="/about">About</RouterLink>
                     <RouterLink class="nav-item px-4 py-2" to="/test">Test</RouterLink>
                     <RouterLink class="nav-item px-4 py-2" to="/products">產品列表</RouterLink>
